09f18779f6db141e6ab4627d8c10bb8c350927ff
RuthAlfredo99 <48080145+RuthAlfredo99@users.noreply.github.com>
2019-03-01 01:10:07 +1400

    Calculadora
    Calculadora

---
+++ Calculadora1/Calculadora1/Form1.Designer.vb
@@ -0,0 +1,242 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Btnmaismenos = New System.Windows.Forms.Button()
+        Me.btnsete = New System.Windows.Forms.Button()
+        Me.Btnquatro = New System.Windows.Forms.Button()
+        Me.Bntum = New System.Windows.Forms.Button()
+        Me.Btnoito = New System.Windows.Forms.Button()
+        Me.Btncinco = New System.Windows.Forms.Button()
+        Me.Btndois = New System.Windows.Forms.Button()
+        Me.Btnzero = New System.Windows.Forms.Button()
+        Me.Btnnove = New System.Windows.Forms.Button()
+        Me.Btnseis = New System.Windows.Forms.Button()
+        Me.Btntres = New System.Windows.Forms.Button()
+        Me.Btnponto = New System.Windows.Forms.Button()
+        Me.Btnresultado = New System.Windows.Forms.TextBox()
+        Me.Btndivisao = New System.Windows.Forms.Button()
+        Me.Btnmais = New System.Windows.Forms.Button()
+        Me.Btnmenos = New System.Windows.Forms.Button()
+        Me.Btnigual = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Btnmaismenos
+        '
+        Me.Btnmaismenos.Location = New System.Drawing.Point(12, 267)
+        Me.Btnmaismenos.Name = "Btnmaismenos"
+        Me.Btnmaismenos.Size = New System.Drawing.Size(59, 33)
+        Me.Btnmaismenos.TabIndex = 0
+        Me.Btnmaismenos.Text = "+-"
+        Me.Btnmaismenos.UseVisualStyleBackColor = True
+        '
+        'btnsete
+        '
+        Me.btnsete.Location = New System.Drawing.Point(12, 115)
+        Me.btnsete.Name = "btnsete"
+        Me.btnsete.Size = New System.Drawing.Size(59, 34)
+        Me.btnsete.TabIndex = 1
+        Me.btnsete.Text = "7"
+        Me.btnsete.UseVisualStyleBackColor = True
+        '
+        'Btnquatro
+        '
+        Me.Btnquatro.Location = New System.Drawing.Point(12, 164)
+        Me.Btnquatro.Name = "Btnquatro"
+        Me.Btnquatro.Size = New System.Drawing.Size(59, 36)
+        Me.Btnquatro.TabIndex = 2
+        Me.Btnquatro.Text = "4"
+        Me.Btnquatro.UseVisualStyleBackColor = True
+        '
+        'Bntum
+        '
+        Me.Bntum.Location = New System.Drawing.Point(12, 216)
+        Me.Bntum.Name = "Bntum"
+        Me.Bntum.Size = New System.Drawing.Size(59, 36)
+        Me.Bntum.TabIndex = 3
+        Me.Bntum.Text = "1"
+        Me.Bntum.UseVisualStyleBackColor = True
+        '
+        'Btnoito
+        '
+        Me.Btnoito.Location = New System.Drawing.Point(77, 115)
+        Me.Btnoito.Name = "Btnoito"
+        Me.Btnoito.Size = New System.Drawing.Size(62, 34)
+        Me.Btnoito.TabIndex = 4
+        Me.Btnoito.Text = "8"
+        Me.Btnoito.UseVisualStyleBackColor = True
+        '
+        'Btncinco
+        '
+        Me.Btncinco.Location = New System.Drawing.Point(77, 164)
+        Me.Btncinco.Name = "Btncinco"
+        Me.Btncinco.Size = New System.Drawing.Size(62, 36)
+        Me.Btncinco.TabIndex = 5
+        Me.Btncinco.Text = "5"
+        Me.Btncinco.UseVisualStyleBackColor = True
+        '
+        'Btndois
+        '
+        Me.Btndois.Location = New System.Drawing.Point(77, 216)
+        Me.Btndois.Name = "Btndois"
+        Me.Btndois.Size = New System.Drawing.Size(62, 36)
+        Me.Btndois.TabIndex = 6
+        Me.Btndois.Text = "2"
+        Me.Btndois.UseVisualStyleBackColor = True
+        '
+        'Btnzero
+        '
+        Me.Btnzero.Location = New System.Drawing.Point(77, 267)
+        Me.Btnzero.Name = "Btnzero"
+        Me.Btnzero.Size = New System.Drawing.Size(62, 33)
+        Me.Btnzero.TabIndex = 7
+        Me.Btnzero.Text = "0"
+        Me.Btnzero.UseVisualStyleBackColor = True
+        '
+        'Btnnove
+        '
+        Me.Btnnove.Location = New System.Drawing.Point(145, 115)
+        Me.Btnnove.Name = "Btnnove"
+        Me.Btnnove.Size = New System.Drawing.Size(56, 34)
+        Me.Btnnove.TabIndex = 8
+        Me.Btnnove.Text = "9"
+        Me.Btnnove.UseVisualStyleBackColor = True
+        '
+        'Btnseis
+        '
+        Me.Btnseis.Location = New System.Drawing.Point(145, 164)
+        Me.Btnseis.Name = "Btnseis"
+        Me.Btnseis.Size = New System.Drawing.Size(56, 36)
+        Me.Btnseis.TabIndex = 9
+        Me.Btnseis.Text = "6"
+        Me.Btnseis.UseVisualStyleBackColor = True
+        '
+        'Btntres
+        '
+        Me.Btntres.Location = New System.Drawing.Point(145, 216)
+        Me.Btntres.Name = "Btntres"
+        Me.Btntres.Size = New System.Drawing.Size(56, 36)
+        Me.Btntres.TabIndex = 10
+        Me.Btntres.Text = "3"
+        Me.Btntres.UseVisualStyleBackColor = True
+        '
+        'Btnponto
+        '
+        Me.Btnponto.Location = New System.Drawing.Point(145, 267)
+        Me.Btnponto.Name = "Btnponto"
+        Me.Btnponto.Size = New System.Drawing.Size(56, 33)
+        Me.Btnponto.TabIndex = 11
+        Me.Btnponto.Text = "."
+        Me.Btnponto.UseVisualStyleBackColor = True
+        '
+        'Btnresultado
+        '
+        Me.Btnresultado.Location = New System.Drawing.Point(12, 48)
+        Me.Btnresultado.Name = "Btnresultado"
+        Me.Btnresultado.Size = New System.Drawing.Size(189, 20)
+        Me.Btnresultado.TabIndex = 12
+        Me.Btnresultado.Text = "TextBox"
+        '
+        'Btndivisao
+        '
+        Me.Btndivisao.Location = New System.Drawing.Point(210, 115)
+        Me.Btndivisao.Name = "Btndivisao"
+        Me.Btndivisao.Size = New System.Drawing.Size(59, 34)
+        Me.Btndivisao.TabIndex = 13
+        Me.Btndivisao.Text = "/"
+        Me.Btndivisao.UseVisualStyleBackColor = True
+        '
+        'Btnmais
+        '
+        Me.Btnmais.Location = New System.Drawing.Point(210, 216)
+        Me.Btnmais.Name = "Btnmais"
+        Me.Btnmais.Size = New System.Drawing.Size(59, 36)
+        Me.Btnmais.TabIndex = 14
+        Me.Btnmais.Text = "+"
+        Me.Btnmais.UseVisualStyleBackColor = True
+        '
+        'Btnmenos
+        '
+        Me.Btnmenos.Location = New System.Drawing.Point(210, 164)
+        Me.Btnmenos.Name = "Btnmenos"
+        Me.Btnmenos.Size = New System.Drawing.Size(59, 36)
+        Me.Btnmenos.TabIndex = 15
+        Me.Btnmenos.Text = "-"
+        Me.Btnmenos.UseVisualStyleBackColor = True
+        '
+        'Btnigual
+        '
+        Me.Btnigual.Location = New System.Drawing.Point(210, 267)
+        Me.Btnigual.Name = "Btnigual"
+        Me.Btnigual.Size = New System.Drawing.Size(59, 33)
+        Me.Btnigual.TabIndex = 16
+        Me.Btnigual.Text = "="
+        Me.Btnigual.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(297, 450)
+        Me.Controls.Add(Me.Btnigual)
+        Me.Controls.Add(Me.Btnmenos)
+        Me.Controls.Add(Me.Btnmais)
+        Me.Controls.Add(Me.Btndivisao)
+        Me.Controls.Add(Me.Btnresultado)
+        Me.Controls.Add(Me.Btnponto)
+        Me.Controls.Add(Me.Btntres)
+        Me.Controls.Add(Me.Btnseis)
+        Me.Controls.Add(Me.Btnnove)
+        Me.Controls.Add(Me.Btnzero)
+        Me.Controls.Add(Me.Btndois)
+        Me.Controls.Add(Me.Btncinco)
+        Me.Controls.Add(Me.Btnoito)
+        Me.Controls.Add(Me.Bntum)
+        Me.Controls.Add(Me.Btnquatro)
+        Me.Controls.Add(Me.btnsete)
+        Me.Controls.Add(Me.Btnmaismenos)
+        Me.Name = "Form1"
+        Me.Text = "Calculadora v 0"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Btnmaismenos As Button
+    Friend WithEvents btnsete As Button
+    Friend WithEvents Btnquatro As Button
+    Friend WithEvents Bntum As Button
+    Friend WithEvents Btnoito As Button
+    Friend WithEvents Btncinco As Button
+    Friend WithEvents Btndois As Button
+    Friend WithEvents Btnzero As Button
+    Friend WithEvents Btnnove As Button
+    Friend WithEvents Btnseis As Button
+    Friend WithEvents Btntres As Button
+    Friend WithEvents Btnponto As Button
+    Friend WithEvents Btnresultado As TextBox
+    Friend WithEvents Btndivisao As Button
+    Friend WithEvents Btnmais As Button
+    Friend WithEvents Btnmenos As Button
+    Friend WithEvents Btnigual As Button
+End Class
---
+++ Calculadora1/Calculadora1/Form1.vb
@@ -0,0 +1,114 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Bntum.Click
+
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Btnnove.Click
+
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Btnseis.Click
+
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Btnigual.Click
+
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Btnmenos.Click
+
+    End Sub
+
+    Private Sub ButtonClickMetod(sender As Object, e As EventArgs) Handles Bntum.Click, Btndois.Click, Btntres.Click, Btnquatro.Click, Btncinco.Click, Btnseis.Click, btnsete.Click, Btnoito.Click, Btnmais.Click, Btnmenos.Click, Btndivisao.Click, Btnmaismenos.Click, Btnresultado.Click,
+        Dim button As Button = CType(sender, Button)
+        If Button.Name = "Bntum" Then
+
+            TextBox.Text = TextBox.Text + "1"
+        End If
+
+        If Button.Name = "Btndois" Then
+
+            TextBox.Text = TextBox.Text + "2"
+        End If
+
+        If Button.Name = "Btntres" Then
+
+            TextBox.Text = TextBox.Text + "3"
+        End If
+
+        If Button.Name = "Btnquatro" Then
+
+            TextBox.Text = TextBox.Text + "4"
+        End If
+
+        If Button.Name = "Btncinco" Then
+
+            TextBox.Text = TextBox.Text + "5"
+        End If
+
+        If Button.Name = "Btnseis" Then
+
+            TextBox.Text = TextBox.Text + "6"
+        End If
+
+        If Button.Name = "Btnsete" Then
+
+            TextBox.Text = TextBox.Text + "7"
+        End If
+
+        If Button.Name = "Btnoito" Then
+
+            TextBox.Text = TextBox.Text + "8"
+        End If
+
+        If Button.Name = "Btnnove" Then
+
+            TextBox.Text = TextBox.Text + "9"
+        End If
+
+        If button.Name = "Btnmaismenos+-" Then
+
+            TextBox.Text = TextBox + "0"
+        End If
+
+        If Button.Name = "Btnzero" Then
+
+            TextBox.Text = TextBox.Text + "."
+        End If
+
+        If button.Name = "OpEquals" Then
+            Dim equation As String = TextBox.Text
+            Dim result = New DataTable().Compute(equation, Nothing)
+            TextBox.Text = result
+        End If
+
+        If button.Name = "OpMultiply" Then
+
+            TextBox.Text = TextBox.Text + "*"
+        End If
+
+        If button.Name = "OpDivide" Then
+
+            TextBox.Text = TextBox.Text + "/"
+        End If
+
+        If button.Name = "OpPlus" Then
+
+            TextBox.Text = TextBox.Text + "+"
+        End If
+
+        If button.Name = "OpMinus" Then
+
+            TextBox.Text = TextBox.Text + "-"
+        End If
+
+        If button.Name = "OpClear" Then
+
+            TextBox.Text = "TextBox.Text"
+        End If
+    End Sub
+End Class
